aa339f1a31951c4a8da85e5e10e6218fa4a9d35c
иван <fairsnake213@gmail.com>
2025-05-13 23:51:00 +0500

    добавил отступ снизу

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  theme: {
-    extend: {
-      padding: {
-        'safe-b': 'env(safe-area-inset-bottom)',
-      },
-    },
-  },
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
