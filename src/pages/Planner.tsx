c11ca3cfbb93c5a762273e608aab86606bbbef88
иван <fairsnake213@gmail.com>
2025-05-16 14:52:26 +0500

    фиксыыыы

--- src/pages/Planner.tsx
+++ src/pages/Planner.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import CalendarPopup from '../features/planner/components/CalendarPopup';
 import DateStrip from '../features/planner/components/DateStrip';
 import TodoList from '../features/planner/TodoList';
@@ -7,15 +6,11 @@ import AddButton from '../features/planner/components/AddButton';
 
 
 function Planner() {
-  const [selectedDate, setSelectedDate] = React.useState<Date>();
   return (
     <div className="p-4">
       <div className="flex gap-2">
       <CalendarPopup />
       <DateStrip
-        daysToShow={7}
-        selectedDate={selectedDate}
-        onDateClick={setSelectedDate}
       />
       </div>
       <HabitTracker />
